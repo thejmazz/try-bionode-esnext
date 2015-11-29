6504512e3372a10a8a600709c15d70c4cd0a4c88
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-11-29 18:12:34 -0500

    async/await implementation of bionode callback

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-async function foo() {
-    console.log('wheee');
+import Promise from 'bluebird';
+import _ from 'lodash';
+import bio from 'bionode';
+
+const ø = Object.create(null);
+
+// Can use bionode modules in 3 different ways:
+
+// 1. The Callback pattern
+/*
+    bio.ncbi.urls('assembly', 'Acromyrmex', function(urls) {
+        console.log(urls[0].genomic.fna)
+    })
+ */
+// reimplemented (quickly) with Promises+Generators=async/await:
+async function bioP(modules, args) {
+    let func = bio;
+    for (let mod of modules) {
+        func = func[mod];
+    }
+
+    return new Promise( (resolve, reject) => {
+        try {
+            func.apply(ø, _(args).concat(resolve).value());
+        } catch(e) {
+            console.error(e);
+        }
+    });
 }
 
-async function bar() {
-    await foo();
+async function runner() {
+    let urls = await bioP(['ncbi', 'urls'], ['assembly', 'Acromyrmex']);
+    console.log(urls[0].genomic.fna);
 }
 
-bar();
+runner();
