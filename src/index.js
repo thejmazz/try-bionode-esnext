7de1da7207bf72c604cf8a1d4620e93d7ff0bf03
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-11-29 18:27:18 -0500

    pipe and event pattern

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Promise from 'bluebird';
 import _ from 'lodash';
+import tool from 'tool-stream';
+import es from 'event-stream';
 import bio from 'bionode';
 
 const ø = Object.create(null);
@@ -30,7 +32,21 @@ async function bioP(modules, args) {
 
 async function runner() {
     let urls = await bioP(['ncbi', 'urls'], ['assembly', 'Acromyrmex']);
-    console.log(urls[0].genomic.fna);
+    console.log(`callback: ${urls[0].genomic.fna}`);
 }
 
 runner();
+
+// 2. The event pattern
+// Too much data at once will crash callbacks, instead get chunks of data
+bio.ncbi.urls('assembly', 'Acromyrmex').on('data', (url) => {
+    console.log(`chunk: ${url.genomic.fna}`);
+});
+
+// 3. The pipe pattern
+bio.ncbi.urls('assembly', 'Acromyrmex')
+    .pipe(tool.extractProperty('genomic.fna'))
+    .pipe(es.through(function write(data) {
+        this.emit('data', `pipe: ${data}\n`);
+    }))
+    .pipe(process.stdout);
